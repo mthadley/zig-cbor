18e4a6eec6af40e014e2995503eec006612265d7
Michael Hadley <mikethadley@gmail.com>
2022-07-23 12:54:14 -0700

    Tests for `uint64_t`

--- src/main.zig
+++ src/main.zig
@@ -29,6 +29,11 @@ test "encode" {
     try t.expectEqualSlices(u8, &[_]u8{ 0b000_11010, 0x00, 0x01, 0x00, 0x00 }, try encode(u32, allocator, 65536));
     try t.expectEqualSlices(u8, &[_]u8{ 0b000_11010, 0x00, 0x12, 0x50, 0xAC }, try encode(u32, allocator, 1_200_300));
     try t.expectEqualSlices(u8, &[_]u8{ 0b000_11010, 0xFF, 0xFF, 0xFF, 0xFF }, try encode(u32, allocator, 4_294_967_295));
+
+    // uint64_t
+    try t.expectEqualSlices(u8, &[_]u8{ 0b000_11011, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00 }, try encode(u64, allocator, 4_294_967_296));
+    try t.expectEqualSlices(u8, &[_]u8{ 0b000_11011, 0x00, 0x00, 0x01, 0x17, 0x77, 0x7A, 0x9F, 0x74 }, try encode(u64, allocator, 1_200_300_400_500));
+    try t.expectEqualSlices(u8, &[_]u8{ 0b000_11011, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF }, try encode(u64, allocator, 0xFFFFFFFFFFFFFFFF));
 }
 
 /// Allocates a buffer and serializes the type using CBOR encoding into it. The caller
